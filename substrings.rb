df118ba9261260775b714b6fbc38a93fc4538633
cyuill <13044041+cyuill@users.noreply.github.com>
2023-01-25 05:52:16 +0000

    Working

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,12 @@
-puts "Hello Odin"
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(word, dictionary)
+
+  word = word.downcase
+  dictionary.each_with_object(Hash.new(0)) do |substring, substring_count|
+    substring_count[substring.downcase] += word.scan(substring.downcase).size
+  end
+end
+
+puts substrings("below it own low", dictionary)
